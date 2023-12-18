46d0c69d3f7d5af9adb6d680cb7a39311f08f697
iQuxLE <ckroll95@gmail.com>
2023-12-18 14:14:01 +0200

    initial commit

---
+++ core/disease_avg_embedding_service.py
@@ -0,0 +1,22 @@
+from chromadb.types import Collection
+
+from core.embedding_service import EmbeddingService
+
+
+class DiseaseAvgEmbeddingService(EmbeddingService):
+    """
+        upsert averaged embeddings from hp_embeddings (cached dict from ont_hp collection) that are connected to the
+        relevant disease from disease_to_hps (cached dict from hpoa) into the disease_avg_embeddings_collection that
+        contains disease and the average embeddings of the correlating hp terms
+    """
+
+    def process_data(self) -> Collection:
+        if not self.disease_to_hps:
+            raise ValueError("disease to hps data is not initialized")
+        if not self.disease_avg_embeddings_collection:
+            raise ValueError("disease_avg_embeddings collection is not initialized")
+        for disease, hps in self.disease_to_hps.items():
+            average_embedding = self.data_processor.calculate_average_embedding(hps, self.hp_embeddings)
+            self.disease_avg_embeddings_collection.upsert(ids=[disease], embeddings=[average_embedding.tolist()],
+                                                          metadatas=[{"type": "disease"}])
+        return self.disease_avg_embeddings_collection
---
+++ core/hp_embedding_service.py
@@ -0,0 +1,24 @@
+from typing import Dict
+
+from core.embedding_service import EmbeddingService
+from core.chromadb_manager import ChromaDBManager
+from core.data_processor import DataProcessor
+from chromadb.types import Collection
+
+
+class HPEmbeddingService(EmbeddingService):
+    def process_data(self) -> Collection:
+        """
+            upsert hps and embeddings into hp_embeddings collection created by chromadbmanager
+        """
+        if not self.hp_embeddings:
+            raise ValueError("HP embeddings data is not initialized")
+        if not self.hp_embeddings_collection:
+            raise ValueError("HP embeddings collection is not initialized")
+        if self.hp_embeddings_collection is not None:
+            for hp_id, data in self.hp_embeddings.items():
+                embedding_list = data['embeddings']
+                self.hp_embeddings_collection.upsert(ids=[hp_id], embeddings=[embedding_list],
+                                                     metadatas=[{"type": "HP"}])
+                # return self?
+        return self.hp_embeddings_collection
